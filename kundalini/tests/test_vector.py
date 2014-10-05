7f923806947956a40a87f3cb6d9cb6c07b7af09b
Cacilhας, La Batalema <batalema@cacilhas.info>
2014-10-05 01:07:15 -0300

    uses numpy

--- kundalini/tests/test_vector.py
+++ kundalini/tests/test_vector.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from numpy import array
 from kundalini import Vector
 
 __all__ = ['TestVector']
@@ -11,36 +12,29 @@ class TestVector(TestCase):
 
 
     def test_simple_2d_vector(self):
-        vector = Vector(3, 4)
-        self.assertEqual(vector, (3, 4, 0, 0))
+        vector = Vector([3, 4])
+        self.assertTrue((vector == array([3, 4])).all())
         self.assertEqual(vector.x, 3)
         self.assertEqual(vector.y, 4)
-        self.assertEqual(vector.z, 0)
-        self.assertEqual(vector.w, 0)
         self.assertEqual(vector.magnitude, 5)
-        self.assertAlmostEqual(vector.angle, self.angle_345)
-        self.assertEqual(vector.angles[1:], (0, 0))
+        self.assertAlmostEqual(vector.angle_xy, self.angle_345)
 
 
     def test_simple_2d_from_angle(self):
-        vector = Vector.from_angle(self.angle_345, magnitude=5)
+        vector = Vector.from_angles((self.angle_345, ), magnitude=5)
         self.assertAlmostEqual(vector.x, 3, places=4)
         self.assertAlmostEqual(vector.y, 4, places=4)
-        self.assertEqual(vector.z, 0)
-        self.assertEqual(vector.w, 0)
 
 
     def test_3d_vector(self):
-        vector = Vector(3, 4, 4)
-        self.assertEqual(vector, (3, 4, 4, 0))
+        vector = Vector([3, 4, 4])
+        self.assertTrue((vector == array([3, 4, 4])).all())
         self.assertEqual(vector.x, 3)
         self.assertEqual(vector.y, 4)
         self.assertEqual(vector.z, 4)
-        self.assertEqual(vector.w, 0)
         self.assertAlmostEqual(vector.magnitude, 6.4031, places=4)
-        self.assertAlmostEqual(vector.angles[0], self.angle_345)
-        self.assertAlmostEqual(vector.angles[1], self.angle_345)
-        self.assertEqual(vector.angles[2], 0)
+        self.assertAlmostEqual(vector.angle_xy, self.angle_345)
+        self.assertAlmostEqual(vector.angle_xz, self.angle_345)
 
 
     def test_3d_from_angle(self):
@@ -49,20 +43,19 @@ class TestVector(TestCase):
         self.assertAlmostEqual(vector.x, 3, places=4)
         self.assertAlmostEqual(vector.y, 4, places=4)
         self.assertAlmostEqual(vector.z, 4, places=4)
-        self.assertEqual(vector.w, 0)
 
 
     def test_4d_vector(self):
-        vector = Vector(3, 4, 4, 4)
-        self.assertEqual(vector, (3, 4, 4, 4))
+        vector = Vector([3, 4, 4, 4])
+        self.assertTrue((vector == array([3, 4, 4, 4])).all())
         self.assertEqual(vector.x, 3)
         self.assertEqual(vector.y, 4)
         self.assertEqual(vector.z, 4)
         self.assertEqual(vector.w, 4)
         self.assertAlmostEqual(vector.magnitude, 7.5498, places=4)
-        self.assertAlmostEqual(vector.angles[0], self.angle_345)
-        self.assertAlmostEqual(vector.angles[1], self.angle_345)
-        self.assertAlmostEqual(vector.angles[2], self.angle_345)
+        self.assertAlmostEqual(vector.angle_xy, self.angle_345)
+        self.assertAlmostEqual(vector.angle_xz, self.angle_345)
+        self.assertAlmostEqual(vector.angle_xw, self.angle_345)
 
 
     def test_4d_from_angle(self):
@@ -75,146 +68,74 @@ class TestVector(TestCase):
 
 
     def test_abs(self):
-        vector = abs(Vector(1, -2, 3))
+        vector = abs(Vector([1, -2, 3]))
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (1, 2, 3, 0))
+        self.assertTrue((vector == array([1, 2, 3])).all())
 
 
     def test_neg(self):
-        vector = -Vector(1, -2, 3)
+        vector = -Vector([1, -2, 3])
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (-1, 2, -3, 0))
-
-
-    def test_pos(self):
-        vector = +Vector(1, -2, 3)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (1, -2, 3, 0))
+        self.assertTrue((vector == array([-1, 2, -3])).all())
 
 
     def test_add(self):
-        vector = Vector(1, 2, 3) + Vector(4, 5, 6)
+        vector = Vector([1, 2, 3]) + Vector([4, 5, 6])
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (5, 7, 9, 0))
+        self.assertTrue((vector == array([5, 7, 9])).all())
 
 
     def test_sub(self):
-        vector = Vector(4, 2, 5) - Vector(1, 5, 0)
+        vector = Vector([4, 2, 5]) - Vector([1, 5, 0])
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (3, -3, 5, 0))
+        self.assertTrue((vector == array([3, -3, 5])).all())
 
 
     def test_mul(self):
-        vector = Vector(1, -2, 3) * 2
+        vector = Vector([1, -2, 3]) * 2
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -4, 6, 0))
+        self.assertTrue((vector == array([2, -4, 6])).all())
 
 
     def test_rmul(self):
-        vector = 2 * Vector(1, -2, 3)
+        vector = 2 * Vector([1, -2, 3])
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -4, 6, 0))
+        self.assertTrue((vector == array([2, -4, 6])).all())
 
 
     def test_truediv(self):
-        vector = Vector(4, -2, 3) / 2
+        vector = Vector([4, -2, 3]) / 2
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -1, 1.5, 0))
+        self.assertTrue((vector == array([2, -1, 1.5])).all())
 
 
     def test_rtruediv(self):
-        vector = 1 / Vector(4, -2, 8)
+        vector = 1 / Vector([4, -2, 8])
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (.25, -.5, .125, 0))
+        self.assertTrue((vector == array([.25, -.5, .125])).all())
 
 
     def test_floordiv(self):
-        vector = Vector(4, -2, 3) // 2
+        vector = Vector([4, -2, 3]) // 2
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -1, 1, 0))
+        self.assertTrue((vector == array([2, -1, 1])).all())
 
 
     def test_mod(self):
-        vector = Vector(13, -3, 26) % 5
+        vector = Vector([13, -3, 26]) % 5
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (3, 2, 1, 0))
+        self.assertTrue((vector == array([3, 2, 1])).all())
 
 
     def _test_divmod(self):
-        vector, rest = divmod(Vector(13, -3, 26), 5)
+        vector, rest = divmod(Vector([13, -3, 26]), 5)
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -1, 5, 0))
+        self.assertTrue((vector == array([1, -1, 5])).all())
         self.assertTrue(isinstance(rest, Vector))
-        self.assertEqual(vector, (3, 2, 1, 0))
+        self.assertTrue((vector == array([3, 2, 1])).all())
 
 
     def test_pow(self):
-        vector = Vector(4, -2, 3) ** 2
+        vector = Vector([4, -2, 3]) ** 2
         self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (16, 4, 9, 0))
-
-
-    def test_lshift(self):
-        vector = Vector(4, -2, 3) << 1
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (8, -4, 6, 0))
-
-
-    def test_rshift(self):
-        vector = Vector(4, -2, 3) >> 1
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (2, -1, 1, 0))
-
-
-    def test_and_1(self):
-        vector = Vector(15, 33) & Vector(8, 32, 1)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (8, 32, 0, 0))
-
-
-    def test_and_2(self):
-        vector = Vector(15, 33) & 8
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (8, 0, 0, 0))
-
-
-    def test_rand(self):
-        vector = 8 & Vector(15, 33)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (8, 0, 0, 0))
-
-
-    def test_or_1(self):
-        vector = Vector(15, 33) | Vector(8, 32, 1)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (15, 33, 1, 0))
-
-
-    def test_or_2(self):
-        vector = Vector(15, 33) | 8
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (15, 41, 0, 0))
-
-
-    def test_ror(self):
-        vector = 8 | Vector(15, 33)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (15, 41, 0, 0))
-
-
-    def test_xor_1(self):
-        vector = Vector(15, 33) ^ Vector(8, 32, 1)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (7, 1, 1, 0))
-
-
-    def test_xor_2(self):
-        vector = Vector(15, 33) ^ 8
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (7, 41, 0, 0))
-
-
-    def test_rxor(self):
-        vector = 8 ^ Vector(15, 33)
-        self.assertTrue(isinstance(vector, Vector))
-        self.assertEqual(vector, (7, 41, 0, 0))
+        self.assertTrue((vector == array([16, 4, 9])).all())
--- kundalini/vector.py
+++ kundalini/vector.py
@@ -1,286 +1,103 @@
 import math
 from numbers import Number
+from numpy import array
 from collections import namedtuple
 
+__all__ = ['Vector']
 
-class Vector(namedtuple('Vector', 'x y z w')):
+ndarray = type(array([]))
 
-    def __new__(cls, x:float=0., y:float=0., z:float=0., w:float=0.) -> namedtuple:
-        return super(Vector, cls).__new__(cls, x, y, z, w)
+
+#-----------------------------------------------------------------------
+class VectorMeta(type):
+
+    def __call__(cls, values):
+        if isinstance(values, (list, tuple)):
+            self = type.__call__(cls, len(values), float)
+            for i, v in enumerate(values):
+                self[i] = v
+
+        else:
+            self = type.__call__(cls, values)
+
+        return self
+
+
+#-----------------------------------------------------------------------
+class Vector(ndarray, metaclass=VectorMeta):
+
+    @property
+    def x(self):
+        return self[0]
 
 
     #---------------------------------------------------------------
     @property
-    def magnitude(self) -> float:
-        if not hasattr(self, '_maginitude'):
-            mag = math.sqrt(pow(self.x, 2) + pow(self.y, 2))
-            mag = math.sqrt(pow(mag, 2) + pow(self.z, 2))
-            self._magnitude = math.sqrt(pow(mag, 2) + pow(self.w, 2))
-        return self._magnitude
-
-
-    #---------------------------------------------------------------
-    @property
-    def angle(self) -> tuple:
+    def angle_xy(self):
         return self.angles[0]
 
 
     #---------------------------------------------------------------
     @property
-    def angles(self) -> tuple:
-        if not hasattr(self, '_angles'):
-            mag = math.sqrt(pow(self.x, 2) + pow(self.y, 2))
-            sin = self.y / mag
-            angles = [math.asin(sin)]
-
-            mag = math.sqrt(pow(self.x, 2) + pow(self.z, 2))
-            sin = self.z / mag
-            angles.append(math.asin(sin))
-
-            mag = math.sqrt(pow(self.x, 2) + pow(self.w, 2))
-            sin = self.w / mag
-            angles.append(math.asin(sin))
-
-            self._angles = tuple(map(math.degrees, angles))
-        return self._angles
+    def y(self):
+        return self[1]
 
 
     #---------------------------------------------------------------
-    @classmethod
-    def from_angle(cls, angle:tuple, *, magnitude:float=1.) -> namedtuple:
-        return cls.from_angles((angle, ), magnitude=magnitude)
+    @property
+    def angle_xz(self):
+        return self.angles[1]
+
+
+    #---------------------------------------------------------------
+    @property
+    def z(self):
+        return self[2]
+
+
+    #---------------------------------------------------------------
+    @property
+    def angle_xw(self):
+        return self.angles[2]
+
+
+    #---------------------------------------------------------------
+    @property
+    def w(self):
+        return self[3]
+
+
+    #---------------------------------------------------------------
+    @property
+    def magnitude(self) -> float:
+        mag = self[0]
+        for v in self[1:]:
+            mag = math.sqrt(pow(mag, 2) + pow(v, 2))
+        return mag
+
+
+    #---------------------------------------------------------------
+    @property
+    def angles(self) -> tuple:
+        angles = [
+            math.asin(v / math.sqrt(pow(self.x, 2) + pow(v, 2)))
+            for v in self[1:]
+        ]
+        return tuple(map(math.degrees, angles))
 
 
     #---------------------------------------------------------------
     @classmethod
     def from_angles(cls, angles:tuple, *, magnitude:float=1.) -> namedtuple:
-        angles += (0, 0, 0)
-        angles = tuple(map(math.radians, angles[:3]))
+        angles = tuple(map(math.radians, angles))
         x = math.cos(angles[0])
         y = math.sin(angles[0])
+        resources = [x, y]
 
-        ax = math.cos(angles[1])
-        z = math.sin(angles[1]) * x / ax
+        for angle in angles[1:]:
+            ax = math.cos(angle)
+            resources.append(math.sin(angle) * x / ax)
 
-        ax = math.cos(angles[2])
-        w = math.sin(angles[2]) * x /ax
-
-        vector = cls(x, y, z, w)
+        vector = cls(resources)
         mag = vector.magnitude
         return vector * magnitude / mag
-
-
-    #---------------------------------------------------------------
-    def __abs__(self) -> namedtuple:
-        return type(self)(
-            x = abs(self.x),
-            y = abs(self.y),
-            z = abs(self.z),
-            w = abs(self.w),
-        )
-
-
-    #---------------------------------------------------------------
-    def __neg__(self) -> namedtuple:
-        return type(self)(
-            x = -self.x,
-            y = -self.y,
-            z = -self.z,
-            w = -self.w,
-        )
-
-
-    #---------------------------------------------------------------
-    def __pos__(self) -> namedtuple:
-        return self
-
-
-    #---------------------------------------------------------------
-    def __add__(self, other:tuple) -> namedtuple:
-        other += (0, 0, 0, 0)
-        return type(self)(
-            x = self.x + other[0],
-            y = self.y + other[1],
-            z = self.z + other[2],
-            w = self.w + other[3],
-        )
-
-    __radd__ = __add__
-
-
-    #---------------------------------------------------------------
-    def __sub__(self, other:tuple) -> namedtuple:
-        other += (0, 0, 0, 0)
-        return type(self)(
-            x = self.x - other[0],
-            y = self.y - other[1],
-            z = self.z - other[2],
-            w = self.w - other[3],
-        )
-
-    def __rsub__(self, other:tuple) -> namedtuple:
-        other += (0, 0, 0, 0)
-        return type(self)(
-            x = other[0] - self.x,
-            y = other[1] - self.y,
-            z = other[2] - self.z,
-            w = other[3] - self.w,
-        )
-
-
-    #---------------------------------------------------------------
-    def __mul__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = self.x * other,
-            y = self.y * other,
-            z = self.z * other,
-            w = self.w * other,
-        )
-
-    __rmul__ = __mul__
-
-
-    #---------------------------------------------------------------
-    def __truediv__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = self.x / other,
-            y = self.y / other,
-            z = self.z / other,
-            w = self.w / other,
-        )
-
-
-    #---------------------------------------------------------------
-    def __rtruediv__(self, other:Number) -> namedtuple:
-        return pow(self, -1) * other
-
-
-    #---------------------------------------------------------------
-    def __floordiv__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = self.x // other,
-            y = self.y // other,
-            z = self.z // other,
-            w = self.w // other,
-        )
-
-
-    #---------------------------------------------------------------
-    def __mod__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = self.x % other,
-            y = self.y % other,
-            z = self.z % other,
-            w = self.w % other,
-        )
-
-
-    #---------------------------------------------------------------
-    def __divmod__(self, other:Number) -> namedtuple:
-        return self // other, self % other
-
-
-    #---------------------------------------------------------------
-    def __pow__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = pow(self.x, other) if self.x else 0,
-            y = pow(self.y, other) if self.y else 0,
-            z = pow(self.z, other) if self.z else 0,
-            w = pow(self.w, other) if self.w else 0,
-        )
-
-
-    #---------------------------------------------------------------
-    def __lshift__(self, other:Number) -> namedtuple:
-        t = type(other)
-        return type(self)(
-            x = int(self.x) << other,
-            y = int(self.y) << other,
-            z = int(self.z) << other,
-            w = int(self.w) << other,
-        )
-
-
-    #---------------------------------------------------------------
-    def __rshift__(self, other:Number) -> namedtuple:
-        return type(self)(
-            x = int(self.x) >> other,
-            y = int(self.y) >> other,
-            z = int(self.z) >> other,
-            w = int(self.w) >> other,
-        )
-
-
-    #---------------------------------------------------------------
-    def __and__(self, other:(Number, tuple)) -> namedtuple:
-        cls = type(self)
-        if isinstance(other, tuple) and len(other) <= 4:
-            other += (0, 0, 0, 0)
-            return cls(
-                x = int(self.x) & int(other[0]),
-                y = int(self.y) & int(other[1]),
-                z = int(self.z) & int(other[2]),
-                w = int(self.w) & int(other[3]),
-            )
-
-        elif isinstance(other, Number):
-            return cls(
-                x = int(self.x) & other,
-                y = int(self.y) & other,
-                z = int(self.z) & other,
-                w = int(self.w) & other,
-            )
-
-        raise TypeError
-
-    __rand__ = __and__
-
-
-    #---------------------------------------------------------------
-    def __or__(self, other:(Number, tuple)) -> namedtuple:
-        cls = type(self)
-        if isinstance(other, tuple) and len(other) <= 4:
-            other += (0, 0, 0, 0)
-            return cls(
-                x = int(self.x) | int(other[0]),
-                y = int(self.y) | int(other[1]),
-                z = int(self.z) | int(other[2]),
-                w = int(self.w) | int(other[3]),
-            )
-
-        elif isinstance(other, Number):
-            return cls(
-                x = int(self.x) | other,
-                y = int(self.y) | other,
-                z = int(self.z) | other if self.z else 0,
-                w = int(self.w) | other if self.w else 0,
-            )
-
-        raise TypeError
-
-    __ror__ = __or__
-
-
-    #---------------------------------------------------------------
-    def __xor__(self, other:(Number, tuple)) -> namedtuple:
-        cls = type(self)
-        if isinstance(other, tuple) and len(other) <= 4:
-            other += (0, 0, 0, 0)
-            return cls(
-                x = int(self.x) ^ int(other[0]),
-                y = int(self.y) ^ int(other[1]),
-                z = int(self.z) ^ int(other[2]) if self.z or other[2] else 0,
-                w = int(self.w) ^ int(other[3]) if self.w or other[3] else 0,
-            )
-
-        elif isinstance(other, Number):
-            return cls(
-                x = int(self.x) ^ other,
-                y = int(self.y) ^ other,
-                z = int(self.z) ^ other if self.z else 0,
-                w = int(self.w) ^ other if self.w else 0,
-            )
-
-        raise TypeError
-
-    __rxor__ = __xor__
